be02e5b9c28189b794094c575015c8b1eadc4bbd
Dash111111 <jshaadan@gmail.com>
2023-05-22 09:15:45 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- *main - Entry point
- *void Return(int);
- *Return; always 0 (success)
- *return 0;
+ * main - Entry point
+ *
+ * Return; always 0 (success)
+ *
  */
 int main(void)
 {
