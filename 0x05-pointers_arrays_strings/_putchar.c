2b37178071929b3fd0e1dc7be888d26518d84529
Dash111111 <jshaadan@gmail.com>
2023-05-30 06:07:04 -0700

    commit

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes character c to stdout
+ * _putchar - write character c to stdout
  * @c: char to be printed
  * Return: On success 1
  */
 
 int _putchar(char c)
 {
-	return (writes(1, &c, 1));
+	return (write(1, &c, 1));
 }
